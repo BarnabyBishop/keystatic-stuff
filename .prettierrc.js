22706f99b893f61f9c06835f910afd28d1543af2
Barnaby Bishop <barneyb@gmail.com>
2023-06-14 13:38:45 +1000

    Updates from core template

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  arrowParens: 'avoid',
+  // printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  proseWrap: 'always',
+  useTabs: false,
+  // FIXME: get this working
+  // importOrder: [
+  //   '^react$',
+  //   '^[a-zA-Z](.*)',
+  //   '^@(.*)',
+  //   '^@(?!voussoir)(.*)$',
+  //   '^[./]',
+  // ],
+  // importOrderSeparation: true,
+  // importOrderSortSpecifiers: true,
+};
